3da2c30c0c450240b857bd7760256cf0105e3d2b
gamerquantannymars <oshiieyov@hotmail.com>
2025-08-16 18:52:17 +0300

    feat: initial commit for khoa-hoc/server.js

---
+++ khoa-hoc/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+app.use(bodyParser.json());
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
+
+app.listen(PORT, () => {
+  console.log(`Khóa học Service running on http://localhost:${PORT}`);
+});
