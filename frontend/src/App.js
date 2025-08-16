e1a9375c2e8de7b9a3bdc7dce525f63d77653664
gamerquantannymars <oshiieyov@hotmail.com>
2025-08-16 18:52:23 +0300

    feat: initial commit for frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hệ Thống Quản Lý Học Phần</h1>
+      <p>Chào mừng bạn đến với hệ thống!</p>
+    </div>
+  );
+}
+
+export default App;
